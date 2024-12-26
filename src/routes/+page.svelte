f63ceb38f890b8cb24fe07acc40029375885a711
McCune1224 <alexmccune1224@gmail.com>
2024-12-26 17:11:23 -0500

    boilerplate for website

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Scrollbar from "./Scrollbar.svelte";
+	const statements: string[] = [
+		"VISITOR DETECTED...",
+		"LOG 26 LOADED",
+		"ACCESS_GRANTED = TRUE;",
+	];
+</script>
+
+<main class="bg-black h-screen text-white">
+	<section class="mx-8 py-9">
+		<Scrollbar {statements} />
+		<h1 class="text-7xl text-center">Welcome Traveller!</h1>
+	</section>
+</main>
---
+++ src/routes/Scrollbar.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	let { statements }: { statements: string[] } = $props();
+</script>
+
+<div class="flex overflow-hidden select-none gap-4 text-white">
+	<ul
+		class="marquee__content flex shrink-0 justify-around min-w-full gap-4 border-2 border-green-500"
+	>
+		{#each statements as statement, i}
+			<li class="">{statement}</li>
+		{/each}
+		<li>EOF</li>
+	</ul>
+	<!-- Mirrors the content above -->
+	<ul
+		class="marquee__content flex shrink-0 justify-around min-w-full gap-4 border-2 border-green-500"
+		aria-hidden="true"
+	>
+		{#each statements as statement, i}
+			<li>{statement}</li>
+		{/each}
+		<li>EOF</li>
+	</ul>
+</div>
+
+<style>
+	.marquee__content {
+		animation: scroll 10s linear infinite;
+	}
+
+	@keyframes scroll {
+		from {
+			transform: translateX(0);
+		}
+		to {
+			transform: translateX(calc(-100% - 1rem));
+		}
+	}
+</style>
